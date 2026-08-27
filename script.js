c952cf8b39c7e8bb235372468feb1fae8e73d627
XanderHurtado <xanderstrong11@gmail.com>
2026-08-27 14:25:08 -0700

    Add event handler for YouTube player state change

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const bannerStatic = document.getElementById("banner-static-container");
 
 let player;
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('banner-video');
+  player = new YT.Player('banner-video',{
+   events:{
+    onPlayerStateChange:pauseBannerVideo
+   }
+  });
 }
 function playBannerVideo() {
   if (!player) return;
@@ -11,4 +15,8 @@ function playBannerVideo() {
   bannerVideo.style.display = "block";
   console.log(player);
   player.playVideo();
+}
+function pauseBannerVideo(event) {
+  if (event.data !== YT.PlayerState.PAUSED) return;
+  console.log("paused");
 }
