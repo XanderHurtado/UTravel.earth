f0e66dae92243159a5f49c6ab35b28188bcbfd43
XanderHurtado <xanderstrong11@gmail.com>
2026-08-27 14:09:51 -0700

    Initialize YouTube player for banner video

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const bannerVideo = document.getElementById("banner-video");
 const bannerStatic = document.getElementById("banner-static-container");
+
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('banner-video');
+}
 function playBannerVideo() {
+  if (!player) return;
   bannerStatic.style.display = "none";
   bannerVideo.style.display = "block";
-  console.log("pressed");
+  player.playVideo();
 }
