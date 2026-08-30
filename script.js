319e373d07dc7f998e161e81f047fdf087f28df0
XanderHurtado <xanderstrong11@gmail.com>
2026-08-30 16:07:36 -0700

    Simplify banner video visibility handling
    Refactor banner video state change logic to use class toggling for visibility.

--- script.js
+++ script.js
@@ -18,15 +18,5 @@ function bannerVideoStateChange(event) {
   let pastIsShowing = isShowingBannerVideo;
   isShowingBannerVideo = event.data === YT.PlayerState.PLAYING || (isShowingBannerVideo && event.data === YT.PlayerState.BUFFERING);
   if (pastIsShowing === isShowingBannerVideo) return;
-  if (isShowingBannerVideo) {
-    bannerStatic.style.opacity = 0;
-    bannerVideo.style.opacity = 1;
-    bannerVideo.style.display= "block";
-  }else{
-    bannerStatic.style.opacity = 1;
-    bannerStatic.style.display = "block";
-    bannerVideo.style.opacity = 0;
-  }
+  bannerVideo.classList.toggle('is-visible');
 }
-bannerVideo.addEventListener( "transitionend", event => {if (event.propertyName === "opacity") bannerVideo .style.display=window.getComputedStyle(bannerVideo ).opacity==="1"?"block":"none"})
-bannerStatic.addEventListener("transitionend", event => {if (event.propertyName === "opacity") bannerStatic.style.display=window.getComputedStyle(bannerStatic).opacity==="1"?"block":"none"})
