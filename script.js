cd90a9f18f15daacc31ead080d805955e8c294c6
XanderHurtado <xanderstrong11@gmail.com>
2026-08-30 14:28:05 -0700

    Fix syntax error in bannerVideoStateChange function

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function playBannerVideo() {
 }
 let isShowingBannerVideo = false;
 function bannerVideoStateChange(event) {
-  if (event.data === YT.PlayerState.PLAYING || (isShowingBannerVideo && event.data === YT.PlayerState.BUFFERING) {
+  if (event.data === YT.PlayerState.PLAYING || (isShowingBannerVideo && event.data === YT.PlayerState.BUFFERING)) {
     bannerStatic.style.display = "none";
     bannerVideo.style.display = "block";
     isShowingBannerVideo = true;
